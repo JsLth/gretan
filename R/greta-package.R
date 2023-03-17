15158f9e5942f72d091624c9a257e31f77bd6e54
Lieth <jonas.lieth@gesis.org>
2023-03-17 16:28:39 +0100

    added package-level documentation and defined global variables

---
+++ R/greta-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+globalVariables(c(
+  "category", "Spatial lag", "Total amount", "Municipality", "Stable income",
+  "Age"
+))
