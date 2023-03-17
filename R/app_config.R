b807b9a80b47fed106a4dcc4cb17e3b12fc391a6
Lieth <jonas.lieth@gesis.org>
2023-03-17 16:25:59 +0100

    reduced some dependencies

--- R/app_config.R
+++ R/app_config.R
@@ -15,35 +15,6 @@ app_sys <- function(...){
 }
 
 
-#' Read App Config
-#' 
-#' @param value Value to retrieve from the config file. 
-#' @param config GOLEM_CONFIG_ACTIVE value. 
-#' If unset, R_CONFIG_ACTIVE.  If unset, "default".
-#' @param use_parent Logical, 
-#' scan the parent directory for config file.
-#' 
-#' @noRd
-get_golem_config <- function(
-    value, 
-    config = Sys.getenv(
-      "GOLEM_CONFIG_ACTIVE", 
-      Sys.getenv(
-        "R_CONFIG_ACTIVE", 
-        "default"
-      )
-    ), 
-    use_parent = TRUE
-){
-  config::get(
-    value = value, 
-    config = config, 
-    # Modify this if your config file is somewhere else:
-    file = app_sys("golem-config.yml"), 
-    use_parent = use_parent
-  )
-}
-
 golem_add_external_resources <- function() {
   golem::add_resource_path("www", app_sys("app/www"))
   tags$head(
