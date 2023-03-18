52311885aadf8ba5dae0ae8958ef1fbc24989c6e
JsLth <jslth@outlook.com>
2023-03-18 02:51:06 +0100

    removed golem dependency

--- R/app_config.R
+++ R/app_config.R
@@ -1,10 +1,5 @@
 #' Access files in the current app
 #' 
-#' NOTE: If you manually change your package 
-#' name in the DESCRIPTION, don't forget to change it here too,
-#' and in the config file. For a safer name change mechanism, 
-#' use the `golem::set_golem_name()` function.
-#' 
 #' @param ... character vectors, specifying subdirectory 
 #' and file(s) within your package. 
 #' The default, none, returns the root of the app.
@@ -15,13 +10,13 @@ app_sys <- function(...){
 }
 
 
-golem_add_external_resources <- function() {
-  golem::add_resource_path("www", app_sys("app/www"))
+add_external_resources <- function() {
+  addResourcePath("www", app_sys("app/www"))
   tags$head(
-    golem::favicon(),
     waiter::useWaiter(),
     shinyWidgets::useSweetAlert(theme = "bootstrap-4"),
     shinyjs::useShinyjs(),
-    includeCSS(app_sys("app/www/styles.css"))
+    includeCSS(app_sys("app/www/styles.css")),
+    tags$link(rel = "shortcut icon", href = app_sys("app/www/favicon.png"))
   )
 }
