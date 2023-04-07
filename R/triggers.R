a2d86dae539f08ba7fda823a3ca29672f0bb054c
JsLth <jslth@outlook.com>
2023-04-07 02:10:12 +0200

    extended trigger functionality

--- R/triggers.R
+++ R/triggers.R
@@ -1,11 +1,26 @@
-init <- function(name, session = getDefaultReactiveDomain()) {
-  session$userData[[name]] <- reactiveVal(FALSE)
+init <- function(..., session = getDefaultReactiveDomain()) {
+  lapply(list(...), function(x) {
+    session$userData[[x]] <- reactiveVal(0)
+  })
 }
 
-trigger <- function(name, session = getDefaultReactiveDomain()) {
-  session$userData[[name]](TRUE)
+trigger <- function(..., session = getDefaultReactiveDomain()) {
+  lapply(list(...), function(x) {
+    session$userData[[x]](session$userData[[x]]() + 1)
+  })
 }
 
 watch <- function(name, session = getDefaultReactiveDomain()) {
   session$userData[[name]]()
+}
+
+on <- function(name, expr, session = getDefaultReactiveDomain()) {
+  if (is.null(session$userData[[name]])) {
+    stop(sprintf("Flag %s has not been initiated: can't listen to it."))
+  }
+  
+  observe({
+    subsitute(expr)
+  }) %>%
+    bindEvent(substitute(watch(name)), ignoreInit = TRUE, env = parent.frame())
 }
