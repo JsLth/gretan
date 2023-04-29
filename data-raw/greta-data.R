178c981c43c02c239344e5bf5884e1da4bc048d2
JsLth <jslth@outlook.com>
2023-04-30 01:48:02 +0200

    added some sugar to data compiler

--- data-raw/greta-data.R
+++ data-raw/greta-data.R
@@ -4,19 +4,24 @@
 # sourced scripts below.
 library(usethis)
 
+old_ws <- ls()
+
 # Create an empty list. Each data-raw file creates a list called output 
 # containing all relevant objects. The lists are appended to sysdata after
 # each source call and stored in R/sysdata.R at the end of the file.
 sysdata <- list()
 
 # First source the data generating scripts for the data explorer
-source("data-raw/bounds.R", local = TRUE)
+cat("Collecting boundary data from GISCO...\n")
+capture.output(source("data-raw/bounds.R", local = TRUE))
 sysdata <- append(sysdata, output) # boundary data
-source("data-raw/georef.R", local = TRUE)
+cat("Georeferencing multinational survey data...\n")
+capture.output(source("data-raw/georef.R", local = TRUE))
 sysdata <- append(sysdata, output) # aggregated survey data + codebook
 
 # Then source the data generating scripts for the case studies
-source("data-raw/coopernico.R", local = TRUE)
+cat("Preparing Coopernico data...\n")
+capture.output(source("data-raw/coopernico.R", local = TRUE))
 sysdata <- append(sysdata, output)
 
 # Prepare data for saving
@@ -25,4 +30,8 @@ sysdata$internal <- TRUE
 sysdata$overwrite <- TRUE
 sysdata$compress <- "bzip2"
 
+# Save data
 do.call(use_data, sysdata)
+
+# Clean workspace
+rm(list = setdiff(ls(), old_ws))
