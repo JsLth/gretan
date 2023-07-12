401c28a87924aedf0e96d120ee6c93afc4afe131
JsLth <jslth@outlook.com>
2023-07-12 03:17:59 +0200

    extended cs1 data preparation

--- data-raw/cs1italy.R
+++ data-raw/cs1italy.R
@@ -1,4 +1,14 @@
-cs_data <- sf::read_sf("data-ext/pilastro_roveri/Use.gpkg") %>%
-  janitor::clean_names()
+library(magrittr)
 
-sf::st_write(cs_data, dsn = "inst/db/cs1italy.gpkg", layer = "buildings")
+db <- "inst/db/cs1italy.gpkg"
+
+fragility <- sf::read_sf("data-ext/pilastro_roveri/fragility in Pilastro-Roveri.shp") %>%
+  dplyr::mutate(nomezona = stringr::str_to_title(nomezona))
+buildings <- sf::read_sf("data-ext/pilastro_roveri/Use.gpkg") %>%
+  janitor::clean_names() %>%
+  dplyr::mutate(dplyr::across(.cols = dplyr::where(is.double), ~round(.x, 2))) %>%
+  dplyr::mutate(property = dplyr::if_else(is.na(property), "Private", property))
+
+sf::st_delete(dsn = db)
+sf::st_write(buildings, dsn = db, layer = "buildings")
+sf::st_write(fragility, dsn = db, layer = "fragility")
