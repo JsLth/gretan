2486e416a059a638bb7b03a3b93e797c6f194549
Jonas Lieth <jonas.lieth@gesis.org>
2023-07-20 17:10:42 +0200

    added a citation wip approach

--- R/cite.R
+++ R/cite.R
@@ -6,5 +6,37 @@ clean_ris <- function(file) {
 }
 
 read_ris <- function(file) {
-  handlr::handl_to_df(handlr::ris_reader(clean_ris(file)))
+  handlr::ris_reader(clean_ris(file))
+}
+
+read_ris <- function(file) {
+  revtools::read_bibliography(file)
+}
+
+create_bib <- function(ref) {
+  revtools::format_citation()
+}
+
+
+create_bib <- function(ref_df) {
+  switch(tolower(resource_type_general),
+    journal = NULL,
+    webpage = bib_software(ref_df)
+  
+  )
+}
+
+bib_software <- function(x) {
+  sprintf(
+    "%s",
+    x$author
+  )
+}
+
+format_author <- function(author) {
+  lapply(author, function(x) {
+    x <- strsplit(x, ", ")[[1]]
+    name <- x[1]
+    first_name <- x[2]
+  })
 }
