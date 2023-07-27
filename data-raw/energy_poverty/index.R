02c2be034e465b866c7b167d7741c4be63f829cb
JsLth <jslth@outlook.com>
2023-07-28 01:23:17 +0200

    updated index.R

--- data-raw/energy_poverty/index.R
+++ data-raw/energy_poverty/index.R
@@ -1,23 +1,31 @@
 library(pacman)
 
 pacman::p_load(
-  "giscoR", "dplyr", "cli", "sf"
+  "giscoR", "dplyr", "cli", "sf", "terra"
 )
 
-if (!exists("nuts3")) {
- nuts3 <- giscoR::gisco_get_nuts(
-   year = "2021",
-   nuts_level = "3",
-   country = countries,
-   epsg = "3035",
-   resolution = "01",
-   spatialtype = "RG",
-   cache = TRUE
- ) %>%
-    st_transform(3035)
-}
+# Get strata
+lau <- giscoR::gisco_get_lau(year = "2020", epsg = "3035", cache = TRUE)
 
-survey_local <- readRDS("survey_local.rds")
+# Get survey data
+survey_local <- readRDS("data-ext/survey_local.rds")
+
+# Get population data
+popgrid <- terra::vect("C:/Users/PC/Downloads/grid_1km_surf.gpkg")
+grid <- terra::rast(
+  popgrid,
+  crs = terra::crs(t),
+  extent = terra::ext(t),
+  resolution = 1
+)
+popgrid <- terra::rasterize(
+  x = popgrid,
+  y = grid,
+  field = "TOT_P_2021",
+  fun = mean,
+  na.rm = TRUE
+)
+
 
 pov <- survey_local %>%
   select(starts_with(c("c16", "c18", "c22", "c24", "c26", "c28",
@@ -28,8 +36,8 @@ cohesion <- survey_local %>%
 trust <- survey_local %>%
   select(starts_with("c47"))
 
-nid3 <- nuts3$nid[st_nearest_feature(survey_local, nuts3)]
-survey_local <- bind_cols(survey_local, nuts3 = nid3)
+lid <- lau$LAU_ID[st_nearest_feature(survey_local, lau)]
+survey_local <- bind_cols(survey_local, lid = lid)
 
 cli::cli_progress_bar(
   name = "Sampling NUTS-3",
@@ -37,14 +45,16 @@ cli::cli_progress_bar(
 )
 cenv <- environment()
 
-survey_jittered <- survey_local %>%
-  group_by(nuts3) %>%
-  group_map(function(x, group) {
-    cli_progress_update(.envir = cenv)
-    name <- group[[1]]
-    st_sample(nuts3[nuts3$nid %in% name, ]$geometry, nrow(x))
-  }) %>%
-  do.call(c, .)
+## Classic approach
+## Single stratified spatial sampling
+# survey_jittered <- survey_local %>%
+#   group_by(nuts3) %>%
+#   group_map(function(x, group) {
+#     cli_progress_update(.envir = cenv)
+#     name <- group[[1]]
+#     st_sample(nuts3[nuts3$nid %in% name, ]$geometry, nrow(x))
+#   }) %>%
+#   do.call(c, .)
 
 gridid <- grid$GRD_ID[st_nearest_feature(survey_local, grid)]
 survey_local <- bind_cols(survey_local, grid = gridid)
