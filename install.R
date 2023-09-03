dce134cd3f02cd9d59a18c12b6926757de2c1b18
JsLth <jslth@outlook.com>
2023-09-03 16:53:11 +0200

    make electricshine depend on forked version

--- install.R
+++ install.R
@@ -1,8 +1,15 @@
-# remotes::install_github("erikvona/electricShine", ref = "fe47ec971ead80c696c99d59984c503f47c381f3")
+if (!require(remotes)) {
+  install.packages("remotes")
+}
 
-library(electricShine)
+if (!require("electricShine")) {
+  remotes::install_github("JsLth/electricShine")
+  library(electricShine)
+}
 
-dir.create("electron")
+if (!dir.exists("electron")) {
+  dir.create("electron")
+}
 
 nodejs <- normalizePath(Sys.which("node"), winslash = "/")
 
@@ -13,12 +20,25 @@ electrify(
   build_path = "electron",
   local_package_path = getwd(),
   function_name = "run_greta",
-  # nodejs_path = gsub(basename(nodejs), "", nodejs),
   nodejs_version = "v14.17.3",
   r_bitness = "x64",
   r_version = "4.3.1",
   cran_like_url = "https://cran.r-project.org",
   license = "GPLv3",
   website = "https://projectgreta.eu/",
-  author = "Jonas Lieth"
+  author = "GESIS - Leibniz Institute for the Social Sciences",
+  keywords = c("GRETA", "Energy Citizenship", "Science"),
+  platforms = list(
+    win = list(
+      target = list(target = "nsis", arch = c("x64", "ia32"))
+    ),
+    mac = list(
+      target = list(target = "default", arch = c("x64", "ia32")),
+      category = "public.app-category.education"
+    ),
+    linux = list(
+      target = list(target = "AppImage", arch = c("x64", "ia32")),
+      category = "Science"
+    )
+  )
 )
