4d59f56134599ce87477683a0a1ccc0f1542d111
JsLth <jslth@outlook.com>
2023-09-08 16:09:21 +0200

    fixed favicon

--- R/app_config.R
+++ R/app_config.R
@@ -23,24 +23,9 @@ add_external_resources <- function() {
     waiter::useWaiter(),
     shinyWidgets::useSweetAlert(theme = "bootstrap-4"),
     shinyjs::useShinyjs(),
-    tags$script(HTML("$('html').on('click', function(e) {
-      if (e.target.id != 'listbox' || e.target.id != 'textSearch') {
-        $('#listbox').hide();
-      }
-    })")),
-    tags$script(HTML("$('html').on('click', function(e) {
-      if (e.target.id == 'textSearch') {
-        $('#listbox').show()
-      }
-    })")),
-    singleton(tags$script(HTML("function scroll_to(id, block) {
-      document.querySelector('#' + id).scrollIntoView({block: block, behavior: 'smooth'});
-    }
-    
-    Shiny.addCustomMessageHandler('scroll', function(opts) {
-      scroll_to(opts.id, opts.block);
-    });"))),
+    tags$script(src = "www/textsearch.js"),
+    tags$script(src = "www/loadingbutton.js"),
     includeCSS(app_sys("app/www/styles.css")),
-    tags$link(rel = "shortcut icon", href = app_sys("app/www/favicon.ico"))
+    tags$link(rel = "shortcut icon", href = "www/favicon.ico")
   )
 }
