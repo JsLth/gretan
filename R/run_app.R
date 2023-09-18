b23f22e508fd6cd4506d11a167c14c1a8ff0dd9c
JsLth <jslth@outlook.com>
2023-09-18 02:04:11 +0200

    updated and documented run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,4 +1,4 @@
-#' Run the "greta" Shiny Application
+#' Run GRETA Analytics
 #'
 #' @examples
 #' \dontrun{
@@ -7,25 +7,31 @@
 #' gretan::run_greta()
 #' }
 #'
-#' @param ... Arguments passed on to \code{\link[shiny]{shinyApp}}
 #' @param log Where to output app logs. Accepts a directory path where a log
-#' file is created. If `""`, prints directly to the console. If `FALSE`,
-#' disables logging. Also accepts a character vector containing module IDs.
-#' In this case, logging is enabled only for these modules.
+#' file is created. If \code{""}, prints directly to the console. If
+#' \code{FALSE}, disables logging. Also accepts a character vector containing
+#' module IDs. In this case, logging is enabled only for these modules.
 #' @param reactlog Whether to enable logging using the reactlog package.
 #' For debugging purposes.
+#' @param set_python Python path to set before starting the app. This should
+#' preferably be done before running the function.
+#' @param console Whether to enable the in-app console. The console can be
+#' accessed by pressing Ctrl + Shift + D and is located in the main module.
+#' For debugging only!
 #' @param options Options for \code{\link[shiny]{shinyApp}}. Included for use
-#' in `electricShine`.
+#' in \code{electricShine}.
+#' @param ... Further options to be passed to the app. Can be accessed using
+#' \code{getGretaOption}.
 #'
 #' @export
 #' @import shiny
 #' @importFrom leaflet %>%
-run_app <- function(...,
-                    log = NULL,
+run_app <- function(log = NULL,
                     reactlog = FALSE,
                     set_python = NULL,
                     console = FALSE,
-                    options = list()) {
+                    options = list(),
+                    ...) {
   if (reactlog) {
     if (!requireNamespace("reactlog")) {
       stop("The package reactlog is required to enable logging.")
@@ -50,8 +56,8 @@ run_app <- function(...,
   }
   
   with_greta_options(
-    shinyApp(ui = app_ui, server = app_server, options = options, ...),
-    options = list(logging = log, console = console)
+    shinyApp(ui = app_ui, server = app_server, options = options),
+    options = list(logging = log, console = console, ...)
   )
 }
 
