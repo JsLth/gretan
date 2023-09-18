fcf06587a723a8feaa421ca70c805940562a4424
JsLth <jslth@outlook.com>
2023-09-18 02:01:48 +0200

    finalized persona preparation script

--- data-raw/persona.R
+++ data-raw/persona.R
@@ -25,8 +25,10 @@ undo_dummy <- function(df, col_prefix) {
 
 
 clusters <- read_csv2("data-ext/persona/persona_clusters.csv") %>%
-  select(uuid = "Respondent code", cluster = cluster_country)
+  select(uuid = "Respondent code", cluster = cluster_country) %>%
+  mutate(cluster = if_else(cluster == 0, 1, cluster))
 
+cb <- readRDS("data-ext/codebook.rds")
 survey_local <- readRDS("data-ext/survey_local.rds") %>%
   select(
     uuid,
@@ -36,7 +38,15 @@ survey_local <- readRDS("data-ext/survey_local.rds") %>%
     all_of(cb[cb$og_var %in% "c12_5", ]$variable),
     all_of(cb[cb$og_var %in% "c68_2", ]$variable)
   ) %>%
-  undo_dummy("c12_5")
+  undo_dummy("c12_5") %>%
+  select(
+    uuid,
+    q1 = c63_1,
+    q2 = c59_1,
+    q3 = c44_2,
+    q4 = c12_5,
+    q5 = c68_2
+  )
 
 dict <- list(
   c12_5_significantly_decrease = 1, c12_5_decrease = 2,
@@ -44,19 +54,22 @@ dict <- list(
   c12_5_significantly_increase = 5, c12_5_prefer_not_to_say = NA_real_
 )
 
-survey_local$c12_5 <- unlist(dict[survey_local$c12_5])
+survey_local$q4 <- unlist(dict[survey_local$q4])
+
+survey_local <- left_join(survey_local, clusters, by = "uuid", multiple = "first")
 
 geom <- st_geometry(survey_local)
 survey_local <- st_drop_geometry(survey_local)
 survey_local <- fastDummies::dummy_cols(
   survey_local,
-  select_columns = setdiff(names(survey_local), "uuid")
+  select_columns = setdiff(names(survey_local), "uuid"),
+  ignore_na = TRUE
 ) %>%
   mutate(across(everything(), .fns = function(.x) {
     if (all(.x %in% c(0, 1, NA))) as.logical(.x) else .x
   })) %>%
   as_tibble()
-survey_local <- left_join(survey_local, clusters, by = "uuid", multiple = "first")
+
 survey_local <- st_sf(survey_local, geometry = geom) %>%
   as_tibble() %>%
   st_as_sf()
@@ -68,13 +81,22 @@ nuts2 <- readRDS("data-ext/bounds/nuts2.rds")
 survey_local <- survey_local %>%
   st_join(select(nuts0, nuts0 = "name"), st_nearest_feature) %>%
   st_join(select(nuts1, nuts1 = "name"), st_nearest_feature) %>%
-  st_join(select(nuts2, nuts2 = "name"), st_nearest_feature)
+  st_join(select(nuts2, nuts2 = "name"), st_nearest_feature) %>%
+  mutate(
+    q1 = if_else(q1 == 8, NA, q1),
+    q4 = if_else(q4 == 8, NA, q4),
+    q5 = if_else(q5 == 6, NA, q5)
+  )
+
+"%len%" <- function(x, y) if (length(x)) x else y 
 
 agg_persona <- function(x) {
   if (is.logical(x)) {
     mean(x, na.rm = TRUE)
   } else if (is.numeric(x)) {
-    median(x, na.rm = TRUE)
+    as.integer(names(sort(table(x), decreasing = TRUE))[1]) %len% NA
+  } else {
+    unique(x)
   }
 }
 
