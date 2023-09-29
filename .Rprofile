e18cf8f57be13f4f290efe078385a563c82686c1
JsLth <jslth@outlook.com>
2023-09-30 01:54:11 +0200

    moved python setup to .Rprofile

--- .Rprofile
+++ .Rprofile
@@ -2,7 +2,15 @@ if (Sys.info()[["user"]] == "shiny") {
   Sys.setenv(PYTHON_PATH = "/usr/bin/python3")
   Sys.setenv(VIRTUALENV_NAME = "gretan")
   Sys.setenv(RETICULATE_PYTHON = "/home/shiny/.virtualenvs/gretan/bin/python")
+  envs <- reticulate::virtualenv_list()
   
+  if (!"gretan" %in% envs) {
+    envdir <- Sys.getenv("VIRTUALENV_NAME")
+    deps <- c("numpy", "lightgbm")
+    reticulate::virtualenv_create(envname = envdir, python = Sys.getenv("PYTHON_PATH"))
+    reticulate::virtualenv_install(envdir, packages = deps, ignore_installed = TRUE)
+    reticulate::use_virtualenv(virtualenv = envdir, required = TRUE)
+  }
 }
 
 Sys.setenv(RSCONNECT_PACKRAT = TRUE)
--- app.R
+++ app.R
@@ -4,22 +4,4 @@
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("app.prod" = TRUE)
-
-if (Sys.info()[["user"]] == "shiny") {
-  cat("User:", Sys.info()[["user"]])
-  cat("Reticulate Python:", Sys.getenv("RETICULATE_PYTHON"))
-  cat("Python path:", Sys.getenv("PYTHON_PATH"))
-  cat("venv name:", Sys.getenv("VIRTUALENV_NAME"))
-  
-  envs <- reticulate::virtualenv_list()
-  
-  if (!"gretan" %in% envs) {
-    envdir <- Sys.getenv("VIRTUALENV_NAME")
-    deps <- c("numpy", "lightgbm")
-    reticulate::virtualenv_create(envname = envdir, python = Sys.getenv("PYTHON_PATH"))
-    reticulate::virtualenv_install(envdir, packages = deps, ignore_installed = TRUE)
-    reticulate::use_virtualenv(virtualenv = envdir,required = TRUE)
-  }
-}
-
 gretan::run_app()
