315cc066f704cc4b1d805be17563bda9e6901e89
JsLth <jslth@outlook.com>
2023-10-15 23:45:26 +0200

    fix slider color and add preliminary dependency download function

--- R/app_config.R
+++ R/app_config.R
@@ -23,7 +23,7 @@ add_external_resources <- function() {
     waiter::useWaiter(),
     waiter::useWaitress(),
     shinyWidgets::useSweetAlert(theme = "bootstrap-4"),
-    shinyWidgets::chooseSliderSkin("Flat", "#fed22b"),
+    shinyWidgets::chooseSliderSkin("Flat"),
     shinyjs::useShinyjs(),
     if (isTRUE(getGretaOption("console", FALSE))) keys::useKeys(),
     tags$script(src = "www/textsearch.js"),
@@ -32,4 +32,24 @@ add_external_resources <- function() {
     includeCSS(app_sys("app/www/styles.css")),
     tags$link(rel = "shortcut icon", href = "www/favicon.ico")
   )
+}
+
+
+download_dependencies <- function(prompt = interactive()) {
+  if (isTRUE(prompt)) {
+    answer <- readline("Install dependencies? [y/N]")
+    if (!identical(answer, "y"))
+      stop("Dependencies needed to start the Shiny app")
+  }
+  
+  plat_db_url <- "https://github.com/TNO/pLAtYpus/raw/main/output/pLAtYpus.sqlite3"
+  plat_alt_url <- "https://github.com/TNO/pLAtYpus/raw/main/output/pLAtYpus_only_survey.sqlite3"
+  plat_db_dest <- app_sys("extdata/stakeholder/output/pLAtYpus.sqlite3")
+  plat_alt_dest <- app_sys("extdata/stakeholder/output/pLAtYpus_only_survey.sqlite3")
+  
+  if (!file.exists(plat_db_dest))
+    download.file(stk_db, destfile = plat_db_dest)
+  
+  if (!file.exists(plat_alt_dest))
+    download.file(stk_alt, destfile = plat_alt_dest)
 }
