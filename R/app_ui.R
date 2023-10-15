449a1bf318ef0c0e199fb745d617d9ab43e5b8a1
JsLth <jslth@outlook.com>
2023-10-16 01:05:32 +0200

    remove fixed footer

--- R/app_ui.R
+++ R/app_ui.R
@@ -99,7 +99,7 @@ app_ui <- function(theme = NULL,
       .tooltip-inner {
         text-align: left;
       }
-
+      
       /* Change carousel arrow color */
       .carousel-control-prev-icon {
         background-image: url(\"data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%2300000' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E\");
@@ -244,13 +244,6 @@ app_ui <- function(theme = NULL,
       collapsed = TRUE,
       expandOnHover = TRUE,
       fixed = FALSE
-    ),
-    footer = bs4Dash::dashboardFooter(
-      right = div(
-        get_text("home", "disclaimer"),
-        style = "font-size: 0.9rem;"
-      ),
-      fixed = TRUE
     ),
     bs4Dash::dashboardBody(
       add_external_resources(),
