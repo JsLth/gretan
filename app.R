20bf5897cbfcb94b66a7128c4fce4e6c8d2e41aa
JsLth <jslth@outlook.com>
2023-10-17 02:38:03 +0200

    move shinyapps setup code back to app.R

--- app.R
+++ app.R
@@ -4,4 +4,26 @@
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("app.prod" = TRUE)
+
+if (Sys.info()[["user"]] == "shiny") {
+  Sys.setenv(PYTHON_PATH = "/usr/bin/python3")
+  Sys.setenv(VIRTUALENV_NAME = "gretan")
+  Sys.setenv(RETICULATE_PYTHON = "/home/shiny/.virtualenvs/gretan/bin/python")
+  envdir <- Sys.getenv("VIRTUALENV_NAME")
+  deps <- c("numpy", "lightgbm", "pLAtYpus_TNO")
+
+  reticulate::virtualenv_create(
+    envname = envdir,
+    python = Sys.getenv("PYTHON_PATH"),
+    version = "3.11"
+  )
+  reticulate::virtualenv_install(
+    envdir,
+    packages = deps,
+    ignore_installed = TRUE,
+    pip_options = "--ignore-requires-python"
+  )
+  reticulate::use_virtualenv(virtualenv = envdir, required = TRUE)
+}
+
 gretan::run_app()
