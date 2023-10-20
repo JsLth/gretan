f6f0148762a626dfba09b57a72f0c3f409c09c19
JsLth <jslth@outlook.com>
2023-10-20 18:22:38 +0200

    add some special code for shinyapps.io

--- app.R
+++ app.R
@@ -2,28 +2,14 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+# force dependencies
+requireNamespace("googledrive")
+requireNamespace("gargle")
+requireNamespace("shinylogs")
+
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+
+# force attaching internal data
+if (!exists("cb_ext")) load("R/sysdata.rda")
 options("app.prod" = TRUE)
-
-if (Sys.info()[["user"]] == "shiny") {
-  Sys.setenv(PYTHON_PATH = "/usr/bin/python3")
-  Sys.setenv(VIRTUALENV_NAME = "gretan")
-  Sys.setenv(RETICULATE_PYTHON = "/home/shiny/.virtualenvs/gretan/bin/python")
-  envdir <- Sys.getenv("VIRTUALENV_NAME")
-  deps <- c("numpy", "lightgbm", "pLAtYpus_TNO")
-
-  reticulate::virtualenv_create(
-    envname = envdir,
-    python = Sys.getenv("PYTHON_PATH"),
-    version = "3.11"
-  )
-  reticulate::virtualenv_install(
-    envdir,
-    packages = deps,
-    ignore_installed = TRUE,
-    pip_options = "--ignore-requires-python"
-  )
-  reticulate::use_virtualenv(virtualenv = envdir, required = TRUE)
-}
-
-gretan::run_app()
+gretan::run_greta()
