2f8b246fcafd929d6dcb63dd7463cfcb180d8d69
JsLth <jslth@outlook.com>
2023-11-17 15:14:44 +0100

    include python in electron setup

--- electron.R
+++ electron.R
@@ -1,11 +1,9 @@
-if (!require(remotes)) {
-  install.packages("remotes")
+if (!require("electricShine")) {
+  if (!require(remotes)) install.packages("remotes")
+  remotes::install_github("JsLth/electricShine")
 }
 
-if (!require("electricShine")) {
-  remotes::install_github("JsLth/electricShine")
-  library(electricShine)
-}
+library(electricShine)
 
 if (!dir.exists("electron")) {
   dir.create("electron")
@@ -17,12 +15,13 @@ if (dir.exists("electron/gretan")) {
 
 nodejs <- normalizePath(Sys.which("node"), winslash = "/")
 
+# Set up electron structure, install nodejs install R and install gretan package
 electrify(
   app_name = "gretan",
   pkg = ".",
   product_name = "GRETA Analytics",
   build_path = "electron",
-  cran_like_url = "https://cran.r-project.org/",
+  cran_like_url = "https://cran.r-project.org",
   function_name = "run_greta",
   nodejs_version = "v14.17.3",
   r_bitness = "x64",
@@ -30,5 +29,27 @@ electrify(
   license = "GPLv3",
   website = "https://projectgreta.eu/",
   author = "GESIS - Leibniz Institute for the Social Sciences",
-  keywords = c("GRETA", "Energy Citizenship", "Science")
+  keywords = c("GRETA", "Energy Citizenship", "Science"),
+  permission = TRUE,
+  run_build = FALSE
+)
+
+# Unpack WinPython
+app_dir <- normalizePath("electron/gretan/app")
+system2("Winpython64-3.11.5.0dot.exe", c(
+  "-y", paste0("-o", normalizePath("electron/gretan/app"))
+))
+file.rename(file.path(app_dir, "WPy64-31150"), file.path(app_dir, "python"))
+
+# Install python dependencies
+system2(
+  file.path(app_dir, "python", "python-3.11.5.amd64", "python.exe"),
+  args = c("-m", "pip", "install", "-r", normalizePath("inst/requirements.txt"))
+)
+
+# Build electron app
+run_build_release(
+  "C:/Users/PC/AppData/Local/R/win-library/4.3/electricShine/nodejs/node-v14.17.3-win-x64",
+  app_path = normalizePath("electron/gretan"),
+  nodejs_version = "v14.17.3"
 )
